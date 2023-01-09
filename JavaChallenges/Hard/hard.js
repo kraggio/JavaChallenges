962ffddf58b5c04ae4e84187392327975f7dc9ba
kraggio <119532707+kraggio@users.noreply.github.com>
2023-01-09 15:04:51 -0500

    Add files via upload

---
+++ JavaChallenges/Hard/hard.js
@@ -0,0 +1,21 @@
+//Define the add function//
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+//Define the subtract function//
+function subtract(num1, num2)
+{
+    return num1 - num2;
+}
+
+//Define the multiply function//
+function multiply(num1, num2)
+{
+    return num1 * num2;
+}
+
+//Define the divide function//
+function divide(num1, num2) {
+    return num1 / num2;
+}
